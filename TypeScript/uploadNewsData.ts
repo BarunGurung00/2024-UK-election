bf0d41e4e569200c8106f22e0f0632bb2e558d1e
Barun <bg440@live.mdx.ac.uk>
2024-04-02 20:36:56 +0100

    Fetching news data and uploading to dynamoDB with analyzer being invoked

--- TypeScript/uploadNewsData.ts
+++ TypeScript/uploadNewsData.ts
@@ -23,7 +23,7 @@ async function findSentiment(text: string): Promise<string> {
 const partyNames: string[] = ["Labour","Conservative","SNP"];
 
 // Calls guardian Api and retrieves array of articles : 10 at a time
-async function fetchNewsApi(name: string): Promise<void> {
+async function fetchNewsApi(name: string): Promise<any> {
     let arrayData = await axios
         .get(
             `https://content.guardianapis.com/search?page=1&q=${name}%20Party%20UK&api-key=5bce4960-e63d-435c-9a1c-5ba2b40a1be1&page-size=2&country=uk`
@@ -31,58 +31,57 @@ async function fetchNewsApi(name: string): Promise<void> {
         .then((data) => {
             return data.data.response.results;
         });
+     
 
-    console.log(arrayData);
-}
+   const region: string ="us-east-1";
 
-async function allPartyNews(): Promise<void> {
-    for (const party of partyNames) {
-        try{
-            let response = await fetchNewsApi(party);
-            console.log(response);
-        } catch(err: any){
-            console.log("Error fetching news: " + err.message);
-        }
-    }
-}
+   const client = new DynamoDBClient({ region });
+   const documentClient = DynamoDBDocumentClient.from(client);
+   
+   
+       for (const data of arrayData) {
 
-const region: string  = "us-east-1";
+           let d =  data.webPublicationDate;
 
-const client = new DynamoDBClient({ region });
-const documentClient = DynamoDBDocumentClient.from(client);
+           const command = new PutCommand({
+               TableName: "sentiment_demo",
+               Item: {
+                   "partyName": "Conservative",
+                   "time_stamp": (new Date(d).valueOf()),
+                   "text": data.webTitle
+               }
+           });
+   
+           try {
+               const response = await documentClient.send(command);
+               console.log(response);
+           } catch (err:any) {
+               console.error("ERROR uploading data Info: " + err.message);
+           }
+       }
+   }
+
+   fetchNewsApi("Conservative");
 
 
-async function putSentiment(): Promise<void>{
-  // Define the parameters for putting the item into the DynamoDB table
-  const params = {
-    TableName: "sentiments",
-    Item: {
-      party_name: "Labour",
-      time_stamp: 1234,
-      impression: "Labour calls for ceasefire"
-    }
-  };
 
-  // Create a new PutItemCommand with the specified parameters
-  const command = new PutCommand(params);
+// async function partyNews(): Promise<void> {
+//     for (const party of partyNames) {
+//         try{
+//             let response = await fetchNewsApi(party);
+//             console.log(response);
+//         } catch(err: any){
+//             console.log("Error fetching news: " + err.message);
+//         }
+//     }    
+// }
 
-  try {
-
-     // Send the PutItemCommand to DynamoDB using the documentClient
-     const response = await documentClient.send(command);
-
-     // Logging the response from DynamoDB
-     console.log(response);
-  } catch (err: any) {
-    console.log("Error putting the data: " + err.message);
-  }
-}
 
 // Call the putSentiment function to execute the operation
 // putSentiment();
 // let d = '2016-01-01T00:00:00.000Z';
 // console.log(new Date(d).valueOf());
 
-findSentiment("Alright").then(data=> {
+// findSentiment("Alright").then(data=> {
 //   console.log(data);
-});
+// });
